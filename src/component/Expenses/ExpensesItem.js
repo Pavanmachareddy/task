3d5d508844d24a3fdb3e89b7b13dac73b4af1631
Pavanmachareddy <pavanrmacha@gmail.com>
2022-07-23 16:17:57 +0530

    Added amount to Expenses

--- src/component/Expenses/ExpensesItem.js
+++ src/component/Expenses/ExpensesItem.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpensesDate from './ExpensesDate';
 import Card from '../UI/Card';
 import './ExpensesItem.css';
 
 const ExpensesItem = (props) => {
-    const clickHandler=(id)=>{
-        props.deleteHandler(props.amount)
+    const [amount,addamount]=useState(props.amount)
+
+    const addExpense=()=>{
+        addamount(amount+100);
+        
     }
+    // const clickHandler=(id)=>{
+    //     props.deleteHandler(props.amount)
+    // }
 
   return (
     <Card className='expense-item'>
       <ExpensesDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Delete Expenses</button>
+        <div className='expense-item__price'>${amount}</div>
+        <button onClick={addExpense}>Add Expenses</button>
+        {/* <button onClick={clickHandler}>Delete Expenses</button> */}
       </div>
     </Card>
   );
